39f9688eca91777fe50d8f01e1748eb84213656e
Kyle Huang <s755105@gmail.com>
2016-07-10 12:30:17 +0800

    kyle commit

--- src/crawler/example/PttHomework.java
+++ src/crawler/example/PttHomework.java
@@ -1,6 +1,9 @@
 package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 
 /**
@@ -14,15 +17,32 @@ import com.github.abola.crawler.CrawlerPack;
 public class PttHomework {
 	
 	public static void main(String[] args) {
-		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1458547807.A.32A.html";
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1468114062.A.3B4.html";
 		
 
-		System.out.println( 
-			CrawlerPack.start()
-			    .addCookie("over18", "1")
-				.getFromHtml(uri)
-				.select(".push-tag:matchesOwn(推) + .push-userid")
-				.toString()
-		);
+//		System.out.println(
+//			CrawlerPack.start()
+//			    .addCookie("over18", "1")
+//				.getFromHtml(uri)
+//					.select("div.push")
+////				.select(".push-tag:matchesOwn(推),.push-userid,.push-content")
+////					.select(".push-tag:matchesOwn(推),.push-tag:matchesOwn(推)+.push-userid")
+////				.select("body:matches()")
+//				.toString()
+//		);
+		Document jsoup=CrawlerPack.start()
+				.addCookie("over18", "1")
+				.getFromHtml(uri);
+		Elements feed=jsoup.select("div.push");
+		System.out.print(feed);
+		for(Element ele :feed){
+			System.out.print(ele.select(".push-tag").text());
+			System.out.print(","+ele.select(".push-userid").text());
+			System.out.print(","+ele.select(".push-content").text());
+			System.out.println();
+		}
+
 	}
+
 }
+
